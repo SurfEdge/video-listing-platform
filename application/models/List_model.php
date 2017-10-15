44f57a44d092c337ed6383b223f4d6b2e7797fb1
Chamath Palihawadana <chamathpali123@gmail.com>
2017-10-15 22:40:16 +0530

    Added List model, connected to the database, image upload added

--- application/models/List_model.php
+++ application/models/List_model.php
@@ -4,9 +4,14 @@ class List_model extends CI_Model {
 
         public $title;
         public $description;
-        public $image;
+        public $cover_image;
         public $author;
         public $vide_list;
+
+        public function __construct()
+        {
+                $this->load->database();
+        }
 
         public function get_last_ten_entries()
         {
@@ -14,13 +19,24 @@ class List_model extends CI_Model {
                 return $query->result();
         }
 
-        public function create_list()
+        public function create_list($image = "")
         {
                 $this->title    = $this->input->post('title');
                 $this->description  = $this->input->post('description');
-                $this->image     = $this->input->post('image');
-                $this->image     = $this->input->post('image');
+                $this->cover_image     = $image;
                 $this->author     = $this->input->post('author');
+
+                $data = array(
+                        'title' => $this->title,
+                        'description' => $this->description,
+                        'cover_image' =>$this->cover_image,
+                        'author_id' =>$this->author
+                );
+
+                $this->db->insert('list', $data);
+                $list_id = $this->db->insert_id();
+                
+                return $list_id;
         }
 
 
